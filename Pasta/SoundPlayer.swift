e5a5ef5dd9d039a223ce0ffcb34e7d1a43f7161f
Katsuya <haradakatsuya190511@gmail.com>
2024-06-16 17:45:25 -0700

    変数名を変更

--- Pasta/SoundPlayer.swift
+++ Pasta/SoundPlayer.swift
@@ -2,43 +2,43 @@ import UIKit
 import AVFoundation
 
 class SoundPlayer: NSObject {
-    var music_data: Data!
-    var pasta_data: Data!
-    var music_player: AVAudioPlayer!
-    var pasta_player: AVAudioPlayer!
+    var musicData: Data!
+    var pastaData: Data!
+    var musicPlayer: AVAudioPlayer!
+    var pastaPlayer: AVAudioPlayer!
     
     override init() {
         do {
-            music_data = NSDataAsset(name: "full")!.data
-            pasta_data = NSDataAsset(name: "pasta")!.data
-            music_player = try AVAudioPlayer(data: music_data)
-            pasta_player = try AVAudioPlayer(data: pasta_data)
+            musicData = NSDataAsset(name: "full")!.data
+            pastaData = NSDataAsset(name: "pasta")!.data
+            musicPlayer = try AVAudioPlayer(data: musicData)
+            pastaPlayer = try AVAudioPlayer(data: pastaData)
         } catch {
             print("load error")
         }
     }
     
     func playMusic() {
-        music_player.play()
+        musicPlayer.play()
     }
     
     func pauseMusic() {
-        music_player?.pause()
+        musicPlayer?.pause()
     }
     
     func stopMusic() {
-        music_player?.stop()
+        musicPlayer?.stop()
     }
     
     func backwardMusic() {
-        music_player?.currentTime -= 10
+        musicPlayer?.currentTime -= 10
     }
     
     func forwardMusic() {
-        music_player?.currentTime += 10
+        musicPlayer?.currentTime += 10
     }
     
     func pasta() {
-        pasta_player.play()
+        pastaPlayer.play()
     }
 }
